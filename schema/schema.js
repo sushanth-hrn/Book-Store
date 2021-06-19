1598de7e463d59ff2e7d5d7195a820d97c4dbdc7
sushanth <sushanth.nukala14@gmail.com>
2021-06-19 15:43:02 +0530

    Type relations (linking authors to list of books)

--- schema/schema.js
+++ schema/schema.js
@@ -5,13 +5,17 @@ const { GraphQLObjectType,
         GraphQLString,
         GraphQLSchema,
         GraphQLID,
-        GraphQLInt
+        GraphQLInt,
+        GraphQLList
 } = graphql;
 
 var books = [
     { name: 'C++ for Beginners', genre: 'Programming', id: '1', authorId: '1'},
     { name: 'It all started with a friend request', genre: 'Love', id: '2', authorId: '2'},
-    { name: 'Java Programming', genre: 'Programming', id: '3', authorId: '3'}
+    { name: 'That\'s the way we met', genre: 'Love', id: '3',authorId: '2'},
+    { name: 'Few things left unsaid', genre: 'Love', id: '4', authorId: '2'},
+    { name: 'Java Programming', genre: 'Programming', id: '5', authorId: '3'},
+    { name: 'Data Structures and Algorithms', genre: 'Programming', id: '6', authorId: '3'}
 ];
 
 var authors = [
@@ -29,7 +33,6 @@ const BookType = new GraphQLObjectType({
         author: {
             type: AuthorType,
             resolve(parent,args){
-                console.log(parent);
                 return lash.find(authors, { id: parent.authorId});
             }
         } 
@@ -41,7 +44,13 @@ const AuthorType = new GraphQLObjectType({
     fields: () => ({
         id: { type:GraphQLID },
         name: { type:GraphQLString },
-        age: { type:GraphQLInt }
+        age: { type:GraphQLInt },
+        books: {
+            type: new GraphQLList(BookType),
+            resolve(parent,args){
+                return lash.filter(books, { authorId: parent.id});
+            }
+        }
     })
 });
 
